07d8873e4633391d5a83294cf9596bff188dad62
Dani <danielgarciagomez@gmail.com>
2014-03-13 12:31:32 +0100

    Fixed case sensitive bug

--- TGD/protected/views/queries/admin.php
+++ TGD/protected/views/queries/admin.php
@@ -7,7 +7,7 @@ $this->breadcrumbs = array(
 
 $this->menu = array(
 		// array('label'=>Yii::t('app', 'Create') . ' ' . $model->label(), 'url'=>array('create')),
-		array('label'=>Yii::t('app', 'Manage') . ' ' . 'Queries Blacklist', 'url' => array('queriesblacklist/admin')),
+		array('label'=>Yii::t('app', 'Manage') . ' ' . 'Queries Blacklist', 'url' => array('queriesBlacklist/admin')),
 	);
 
 
