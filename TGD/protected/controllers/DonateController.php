056caeae09cc516504effe0663fd06728b3366fa
Dani <danielgarciagomez@gmail.com>
2014-04-02 15:22:23 +0200

    Added donate controller and view

---
+++ TGD/protected/controllers/DonateController.php
@@ -0,0 +1,120 @@
+<?php
+
+class DonateController extends Controller {
+
+    const INIT = 1;
+    const RETURN_FROM_GATEWAY = 2;
+
+    const TRANSACTION_OK = 'OK';
+    const TRANSACTION_ERROR = 'ERROR';
+
+    const TYPE_LOAN = "LOAN";
+    const TYPE_SHARE = "SHARE";
+    const TYPE_DONATION = "DONATION";
+
+    public $transaction_id = '';
+    public $user_id = 0;
+    public $type ='';
+    public $status ='';
+    public $currency='';
+    public $amount='';
+
+    public $bodyId = 'tgd-user-data';
+
+    public function init() {
+
+        $this->layout = '//layouts/blank';
+        Yii::app()->theme = 'tgd';
+
+        // set title
+        $this->pageTitle = " - Donate";
+
+        // set body id to #tgd-donate
+        $this->bodyId = "tgd-donate";
+        
+        // add js specific for this page
+        Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl . '/js/share_purchase.js', CClientScript::POS_END);
+        
+        parent::init();
+    }
+
+    // public function filters() {
+    //     return array('accessControl'); // perform access control for CRUD operations
+    // }
+
+    public function accessRules() {
+        return array(
+            array('allow', // allow authenticated users to access all actions
+                'users' => array('@'),
+            ),
+            array('deny'),
+        );
+    }
+
+    public function actionResponse($user_token,$status,$transaction_id,$type) {
+
+        
+    }
+
+    public function actionIndex() {
+
+        if (isset(Yii::app()->user)){
+            $this->user_id = Yii::app()->user->id;
+        }
+
+
+        if (isset($_GET['token']) && $_GET['token']!='')
+        {
+            $token = $_GET['token'];
+
+            
+
+            $token = base64_decode($token);
+            $token = json_decode( $token);
+
+            $this->type = $token->type;
+            $this->transaction_id = $token->transaction_id;
+            $this->status = $token->status;
+            $this->currency = $_GET['currency'];
+            $this->amount = (int)$_GET['amount'] / 100;
+            
+            if ( Transactions::model()->find('transaction_id=:id_transaction', array(':id_transaction'=>$this->transaction_id)) == null)
+            {
+                $transaction = new Transactions();
+                $transaction->transaction_id=$this->transaction_id;
+                $transaction->type=$this->type;
+                $transaction->status=$this->status;
+                $transaction->currency=$this->currency;
+                $transaction->amount=$this->amount;
+                $transaction->member_id=$this->user_id;
+                $transaction->save();
+            }
+
+
+            $this->render('index', array(
+                'transaction_id'=>$this->transaction_id,
+                'state' => DonateController::RETURN_FROM_GATEWAY,
+                'status' => $this->status,
+                )
+            );
+
+        }
+        else
+        {
+
+            $this->transaction_id = uniqid();
+
+            //var_dump($user);die;
+
+            $this->render('index', array(
+                'transaction_id'=>$this->transaction_id,
+                'state' => DonateController::INIT
+                )
+            );
+
+        }
+
+        
+    }
+
+}
---
+++ TGD/protected/views/donate/index.php
@@ -0,0 +1,163 @@
+
+<div id="tgd-page-title">
+    <div class="container">
+        <div class="row">
+            <ul class="clearfix">
+                <li><a href="/"><span class="fa fa-home"></span></a></li>
+                <li><h2>Donate</h2></li>
+            </ul>
+        </div>
+    </div>
+</div> 
+
+<?php if ($state == DonateController::RETURN_FROM_GATEWAY && $status == DonateController::TRANSACTION_OK) { ?>   
+    <div class="alert alert-success">SUCCESS</div>
+<?php } ?>
+
+<?php if ($state == DonateController::RETURN_FROM_GATEWAY && $status == DonateController::TRANSACTION_ERROR) { ?>   
+    <div class="alert alert-danger">ERROR</div>
+<?php } ?>
+
+<div id="tgd-page-content" class="container">
+    <div class="row">
+        <div class="col-sm-16 col-md-10 col-lg-10 tgd-no-horizontal-padding">
+            <section id="descriptions">
+                <div>
+                    <h2>Please, support our mission!</h2>
+                    <p>Are you a generous person that want to take the opportunity to support us with a donation? don't be shy! We accept credit cards and bitcoins. Every little help is apreciated.</p>
+                </div>
+                <div class="clearfix">
+                    <div class='text'>
+                        Click on this button if you wish to pay via credit card.
+                    </div>
+                    <div class="button">
+                        <button class="btn credit-card disabled" data-toggle="credit-card">Donate with<br> credit card</button>
+                    </div>
+                </div>
+                <div class="clearfix">
+                    <div class="text">
+                        We accept bitcoins as well. Click on this button if this
+                        method is more of your liking.
+                    </div>
+                    <div class="button">
+                        <button class="btn bitcoin" data-toggle="bitcoin">Donate with<br> bitcoins</button>
+                    </div>
+                </div>
+                <div id="questions">
+                    <a href="<?php echo Yii::app()->controller->createAbsoluteUrl("/site/faq") ?>">Have questions?</a>
+                </div>
+            </section>
+        </div>
+        <div class="col-sm-16 col-md-6 col-lg-6 tgd-no-horizontal-padding">
+            <section id="form">
+                <div id="credit-card">
+                    <strong>Choose a fair amount</strong>
+                    Thanks for taking this generous decision. Use the form fields bellow
+                    to specify a fair amount and the currency in which you wish to make the donation.
+                    <form id="donate" action="" method="post" onsubmit="return bp.validateMobileCheckoutForm($('#donate'));">
+                        <input name="action" type="hidden" value="checkout">
+                        <div class="form-group row">
+                            <div class="col-md-8 tgd-no-left-padding">
+                                <input class="form-control" name="price" type="number" placeholder="Amount" value="10.00" />
+                            </div>
+                            <div class="col-md-8 tgd-no-right-padding">
+                                <select class="form-control" name="currency" value="" >
+                                    <option value="USD" selected="selected">USD</option>
+                                    <option value="BTC">BTC</option>
+                                    <option value="EUR">EUR</option>
+                                    <option value="GBP">GBP</option>
+                                </select>
+                            </div>
+                        </div>                        
+                        <button type="submit" class="btn btn-primary" disabled="disabled">donate! <span class="fa fa-credit-card"></span></button>
+
+                        
+                    </form>
+                </div>
+                <div id="bitcoin">
+                    <strong>Choose a fair amount</strong>
+                    Thanks for taking this generous decision. Use the form fields bellow
+                    to specify a fair amount and the currency in which you wish to make the donation.
+                    <form id="donate_bitcoin" action="https://bitpay.com/checkout" method="post" onsubmit="return bp.validateMobileCheckoutForm($('#donate'));">
+                        <input name="action" type="hidden" value="checkout">
+                        <div class="form-group row">
+                            <div class="col-md-8 tgd-no-left-padding">
+                                <input id="amount" class="form-control" name="price" type="number" placeholder="Amount" value="10.00" />
+                            </div>
+                            <div class="col-md-8 tgd-no-right-padding">
+                                <select id="currency" class="form-control" name="currency" value="aa" >
+                                    <option value="USD" selected="selected">USD</option>
+                                    <option value="BTC">BTC</option>
+                                    <option value="EUR">EUR</option>
+                                    <option value="GBP">GBP</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <input type="hidden" name="redirectURL" value="" />
+                        <input type="hidden" name="data" value="i+8wYLNGk9LIgh913da5SgG/GA7jj8JhA0LTzmCuHJ2A1xPgE/JABmoEwVawN+bsUjzwXKkU5syz/IelMYuZhsb5J6oKZ6n2CQ+hWvueNa4vp3vH7dx9PPgD5/KRTGkqSP568lr7CJ3UecrKSbQpBY0UT6wVK17gIVG98wp8Ld0hEcf0dFKpN/ZFk8T75wsDfnk0dCvnUnDOdEmdWS0aCg52y8OKJJzLNq+o2OaM8Kw=">
+                        <button type="submit" id="btnProceedDonationBitcoin" class="btn btn-primary">donate <span class="fa fa-bitcoin"></span></button>
+
+                        <script>
+                        $( document ).ready(function() {
+
+                            // $( "#btnProceedDonationCC" ).click(function() {
+                            //    alert('CC');
+                            //    return false;
+                            // });
+
+                            $( "#btnProceedDonationBitcoin" ).click(function() {
+                                
+                               $('#donate_bitcoin input:hidden[name="redirectURL"]').val('<?php 
+
+                                $param = array(
+                                    'transaction_id'=>$transaction_id,
+                                    'type'=>DonateController::TYPE_DONATION,
+                                    'status'=>DonateController::TRANSACTION_OK
+                                );
+                                
+                                $param_json=json_encode($param);
+                                $param_b64 = base64_encode($param_json);
+
+                                echo Yii::app()->controller->createAbsoluteUrl('donate/?token='.$param_b64.'&currency=[currency]&amount=[amount]');
+
+                                ?>'); 
+
+                               var currency=$('#currency').find(":selected").val();
+                               
+                               var amount=parseInt($('#amount').val()) * 100;
+
+                               var redirectURL=$('#donate_bitcoin input:hidden[name="redirectURL"]').val();
+                               
+
+                               redirectURL = redirectURL.replace("[currency]",currency);
+                               redirectURL = redirectURL.replace("[amount]",amount);
+
+                               
+                               $('#donate_bitcoin input:hidden[name="redirectURL"]').val(redirectURL);
+
+                               
+                               
+                            });
+                        });
+                        </script>
+                    </form>
+                </div>
+            </section>
+        </div>
+    </div>
+</div>
+<script>
+    $('#descriptions button').not(':disabled').click(function() {
+        var targetId = '#' + $(this).data('toggle'),
+                $target = $(targetId),
+                $parent = $target.parent();
+
+        if (!$target.is(':visible')) {
+            // hide the rest
+            $parent.children().not(targetId).hide();
+            // show the target
+            $target.show();
+        }
+    });
+</script>
