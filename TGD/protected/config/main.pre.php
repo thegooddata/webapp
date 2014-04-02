d7211d998db63396a4b92dc2e2451bbd4ef0c279
Dani <danielgarciagomez@gmail.com>
2014-04-02 15:48:10 +0200

    Added rule

--- TGD/protected/config/main.pre.php
+++ TGD/protected/config/main.pre.php
@@ -152,6 +152,9 @@ return array(
 		        array('api/delete', 'pattern'=>'api/<model:\w+>/<id:\d+>', 'verb'=>'DELETE'),
 		        array('api/create', 'pattern'=>'api/<model:\w+>', 'verb'=>'POST'),
 		        
+		        array('purchase/index', 'pattern'=>'user/purchase/<user_token:[a-zA-Z0-9+]+={0,2}>', 'verb'=>'GET'),
+		        //array('purchase/response', 'pattern'=>'user/purchase/<user_token:[a-zA-Z0-9+]+={0,2}>/<token:[a-zA-Z0-9+]+={0,2}>', 'verb'=>'GET'),
+		        
 		        //Set friendly-url
 		        '/' => 'site/index',
 
