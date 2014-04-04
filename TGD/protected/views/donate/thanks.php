0fff4ec38fdf897f8f986b8c18aed1f7d9d8dbab
Juan Menéndez <juan.menendez.buitrago@gmail.com>
2014-04-04 19:20:16 +0200

    "Thanks" pages for donation and share purchases.

---
+++ TGD/protected/views/donate/thanks.php
@@ -0,0 +1,30 @@
+
+
+<div id="tgd-page-title">
+    <div class="container">
+        <div class="row">
+            <ul class="clearfix">
+                <li><a href="/"><span class="fa fa-home"></span></a></li>
+                <li><h2>Donate</h2></li>
+            </ul>
+        </div>
+    </div>
+</div> 
+<div class="wrapper">
+    <div id="tgd-page-content" class="container thanks">
+        <div class="row">
+            <div class="col-sm-offset-1 col-sm-14 tgd-no-horizontal-padding">
+                <div>
+                    <h1>Thanks for supporting TheGoodData!</h1>
+                    <p>Your payment has been processed successfully.</p>
+                    <h2>You can also support us in other ways</h2>
+                    <ul>
+                        <li>Install TheGoodData extension</li>
+                        <li>Tell your family, friends, and colleagues about TheGoodData by email, facebook, twitter or google plus</li>
+                        <li>Become a member of TheGoodData and participate in our collaborative platform</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ TGD/protected/views/purchase/thanks.php
@@ -0,0 +1,27 @@
+
+
+<div id="tgd-page-title">
+    <div class="container">
+        <div class="row">
+            <ul class="clearfix">
+                <li><a href="/"><span class="fa fa-home"></span></a></li>
+                <li><h2>Donate</h2></li>
+            </ul>
+        </div>
+    </div>
+</div> 
+<div class="wrapper">
+    <div id="tgd-page-content" class="container thanks">
+        <div class="row">
+            <div class="col-sm-offset-1 col-sm-14 tgd-no-horizontal-padding">
+                <div>
+                    <h1>You are now owner of one of our shares!</h1>
+        <p>From now own you are a full right Member of TheGoodData!</p>
+        <h1>You are invited to collaborate with us</h1>
+        <p>Please visit <a href="https://collaborate.thegooddata.org/">https://collaborate.thegooddata.org/</a> to join the discussion about key company topics and be informed about business progress.</p>
+        <p>TheGoodData is an open and collaborative company, and its goals will only be achievable with our transparency and your participation.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
