a43d9796c0ed879516234e8f29897e39ec6e126f
Dani <danielgarciagomez@gmail.com>
2014-04-09 16:45:02 +0200

    Remove access control  to good data controller

--- TGD/protected/controllers/GoodDataController.php
+++ TGD/protected/controllers/GoodDataController.php
@@ -5,15 +5,15 @@ class GoodDataController extends Controller {
     public $bodyId = 'tgd-good-data';
 
     public function filters() {
-        return array('accessControl'); // perform access control for CRUD operations
+        //return array('accessControl'); // perform access control for CRUD operations
     }
 
     public function accessRules() {
         return array(
-            array('allow', // allow authenticated users to access all actions
-                'users' => array('@'),
-            ),
-            array('deny'),
+            // array('allow', // allow authenticated users to access all actions
+            //     'users' => array('@'),
+            // ),
+            // array('deny'),
         );
     }
 
