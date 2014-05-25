68f5d9e5302802258be36d5a00c560f3f36f50c7
rgalindo33 <rgalindo33@hotmail.com>
2014-05-26 01:15:45 +0200

    border line colour change

--- TGD/themes/tgd/js/share_purchase.js
+++ TGD/themes/tgd/js/share_purchase.js
@@ -7,3 +7,12 @@ $(function() {
 
     setTimeout(function(){sameSize();},100);
 });
+
+$(document).ready(function(){
+  $('#descriptions .btn').click( function( e ){
+    var dataToggle = $(this).data('toggle');
+    console.log(dataToggle);
+    $('#descriptions').addClass( dataToggle );
+  });
+
+});
