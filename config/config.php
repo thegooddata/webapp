179a6893306bae9c5a11590cbd0cde087c9923b7
rgalindo33 <rgalindo33@hotmail.com>
2014-05-25 23:58:52 +0200

    added level icons and proper function

---
+++ config/config.php
@@ -0,0 +1,31 @@
+<?php
+
+CONST REDOCTOBER_URL = 'https://localhost:8080';
+CONST REDOCTOBER_PORT = '8080';
+CONST REDOCTOBER_USERNAME = 'user';
+CONST REDOCTOBER_PASSWORD = 'pass';
+CONST REDOCTOBER_OWNERS = '"user1","user2"';
+CONST REDOCTOBER_MIN = 2;
+CONST REDOCTOBER_CERT = "/Users/you/Dev/redoctober/cert/server.crt";
+
+CONST BD_NAME = 'tgd2';
+CONST BD_HOST = 'localhost';
+CONST BD_USERNAME = 'postgres';
+CONST BD_PASSWORD = 'perrovaca';
+CONST BD_PORT = 5432;
+
+const EMAIL_ADMIN = 'admin@thegooddata.com';
+
+CONST EMAIL_GENERIC_FROM = 'info@thegooddata.org';
+CONST EMAIL_GENERIC_HOST = "smtp.zoho.com";
+CONST EMAIL_GENERIC_USERNAME = 'info@thegooddata.org';
+CONST EMAIL_GENERIC_PASSWORD = 'password';
+CONST EMAIL_GENERIC_PORT = '465';
+
+CONST EMAIL_PERSONAL_FROM = 'you@thegooddata.org';
+CONST EMAIL_PERSONAL_HOST = "smtp.zoho.com";
+CONST EMAIL_PERSONAL_USERNAME = 'you@thegooddata.org';
+CONST EMAIL_PERSONAL_PASSWORD = 'password';
+CONST EMAIL_PERSONAL_PORT = '465';
+
+?>
