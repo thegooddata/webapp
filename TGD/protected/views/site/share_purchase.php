d0b36596efd1496abc4d0a93638ed36ab75e5969
rgalindo33 <rgalindo33@hotmail.com>
2014-07-18 15:12:42 +0200

    add logged menu to share purchase

--- TGD/protected/views/site/share_purchase.php
+++ TGD/protected/views/site/share_purchase.php
@@ -1,11 +1,3 @@
-
-<div id="tgd-page-title">
-    <div class="container">
-        <div class="row">
-            <h2>Share purchase</h2>
-        </div>
-    </div>
-</div> 
 <div class="wrapper">
     <div id="tgd-page-content" class="container">
         <div class="row">
