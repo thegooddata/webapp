b3761d0f3a3bb7505f605a0b51dc25c5d62487c7
Juan Menendez <juan.menendez.buitrago@gmail.com>
2014-09-15 13:21:07 +0200

    Registration 'thanks' page.

---
+++ TGD/protected/modules/user/views/registration/thanks.php
@@ -0,0 +1,28 @@
+<div class="wrapper">
+    <div id="tgd-page-title">
+        <div class="container">
+            <div class="row">
+                <h2>Application submitted</h2>
+            </div>
+        </div>
+    </div> 
+    <div id="tgd-page-content" class="container thanks">
+        <div class="row">
+            <div class="col-sm-offset-1 col-sm-14 tgd-no-horizontal-padding">
+                <div>
+                    <h2>Thanks! Now we have to review your application...</h2>
+                    <p>As you can imagine, becoming a company owner is a bit trickier than registering to a service!</p>
+                    <p>We just need to check the details that you have submitted and, if everything looks ok, we will send you an email inviting you to become a member. We will do our best to do it in one working day.</p>
+                    <p>In the meantime you can keep on using TheGoodData extension.</p>
+                    <p>Should you have any question about your application, please contact <a href="mailto:members@thegooddata.org">members@thegooddata.org</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
