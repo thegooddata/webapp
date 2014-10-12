e1f140b1652edb3ee4682dd474bd02232e839a0a
Juan Menendez <juan.menendez.buitrago@gmail.com>
2014-10-12 21:22:19 +0200

    Moved helper functions from helpers.php.

--- TGD/protected/helpers/ADbHelper.php
+++ TGD/protected/helpers/ADbHelper.php
@@ -65,12 +65,119 @@ class ADbHelper {
     return true;
   }
   
+  /**
+   * getPercentile
+   *
+   * Finds the percentile of a given user/member.
+   *
+   * @param mixed $id The user ID (string) or member ID (integer)
+   * @return float The percentile of the user.
+   */
   static public function getPercentile($id) {
-    
+    $member_id = $id;
+    if (!is_numeric($member_id))
+    {
+      $member_id = 0;
+    }
+
+    if ($member_id != 0)
+    {
+      $datas = Yii::app()->db->createCommand()
+          ->setFetchMode(PDO::FETCH_OBJ)
+          ->select('percentile')
+          ->from('view_queries_members_percentil')
+          ->where(array(
+                      'and',
+                      'member_id = :member_id'
+                      ),
+                      array(
+                          'member_id'=>$member_id)
+                      )
+          ->queryAll();
+
+        if (count($datas)>0)
+        {
+          return $datas[0]->percentile;
+        }
+      else
+      {
+        return 0;
+      }
+      
+    }
+    else
+    {
+      $user_id = $id;
+      $datas = Yii::app()->db->createCommand()
+          ->setFetchMode(PDO::FETCH_OBJ)
+          ->select('percentile')
+          ->from('view_queries_users_percentil')
+          ->where(array(
+                      'and',
+                      'user_id = :user_id'
+                      ),
+                      array(
+                          'user_id'=>$user_id)
+                      )
+          ->queryAll();
+
+        if (count($datas)>0)
+        {
+          return $datas[0]->percentile;
+        }
+      else
+      {
+        return 0;
+      }
+    }
   }
   
+  /**
+   * getSeniorityLevelAndPercentile
+   *
+   * Gets the seniority level and percentile of a given user.
+   *
+   * @param mixed $id the member ID or user ID.
+   * @return array An associative array containing the level and percentile.
+   */
   static public function getSeniorityLevelAndPercentile($id) {
-    
+      
+    $percentile = $this->getPercentile($id);
+    $result = array('value' => $percentile, 'level' => '');
+
+    $status = Yii::app()->user->user($id)->status;
+
+    if(!Yii::app()->user->isGuest && $status == 2)
+    {
+      if(false /* TODO: find out a way to figure out if the user is a collaborator*/)
+      {
+        $result['level'] = 'Collaborator';
+      }
+      else
+      {
+        if($percentile < 20)
+        {
+          $result['level'] = 'Owner';
+        }
+        else
+        {
+          $result['level'] = 'Expert';
+        }
+      }
+    }
+    else
+    {
+      if($percentile < 5)
+      {
+        $result['level'] = 'Apprentice';
+      }
+      else
+      {
+        $result['level'] = 'Journeyman';
+      }
+    }
+
+    return $result;
   }
 }
 ?>
