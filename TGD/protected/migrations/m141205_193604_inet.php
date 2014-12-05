2d78b76a2219c5d6f41fb4b9a810bc64e7941ef4
Alexandru Trandafir Catalin <atrandafirc@gmail.com>
2014-12-05 20:42:51 +0100

    store user ip address in active users

---
+++ TGD/protected/migrations/m141205_193604_inet.php
@@ -0,0 +1,26 @@
+<?php
+
+class m141205_193604_inet extends CDbMigration
+{
+	public function up()
+	{
+      $this->execute("ALTER TABLE tbl_active_users ADD COLUMN host inet SET DEFAULT NULL");
+	}
+
+	public function down()
+	{
+		echo "m141205_193604_inet does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
