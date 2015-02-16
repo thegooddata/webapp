2fd1212c0ea8be54499fb0477a22ea42deaccf36
Juan Menendez <juan.menendez.buitrago@gmail.com>
2015-02-16 19:44:51 +0100

    #49: Console command that makes an REST request to Yahoo! finance API in order to get the exchange rate for two currency pairs passed by parameter (EURUSD, USDGBP ... etc).

---
+++ TGD/protected/commands/GetPairRateCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+class GetPairRateCommand extends CConsoleCommand{
+	public function actionIndex($pair, $format='json', $json=false) {
+	
+		$yahoo_query = 'select * from yahoo.finance.xchange where pair in ("'.$pair.'")';
+		$env = 'store://datatables.org/alltableswithkeys';
+		$service_url = 'https://query.yahooapis.com/v1/public/yql?q='.urlencode($yahoo_query).'&format='.$format.'&env='.urlencode($env).'&callback=';
+		
+		$curl = curl_init();
+		// Deal with SSL
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+
+		curl_setopt($curl, CURLOPT_URL, $service_url);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$curl_response = curl_exec($curl);
+
+		$httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+		curl_close($curl);
+
+		if($httpcode == 200){
+			$curl_response = json_decode($curl_response);
+			if($json === true){
+				return json_encode($curl_response->query->results->rate);
+			}else{
+				return $curl_response->query->results->rate->Rate;
+			}
+		}else{
+			return false;
+		}
+	}
+}
+
