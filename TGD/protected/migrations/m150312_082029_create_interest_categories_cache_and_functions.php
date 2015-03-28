f76b5336d9c892e2922eb014b76d963f937076f5
alexandru <alexandru@192.168.1.43>
2015-03-28 18:44:04 +0100

    fix migration issue that was calling a function before having created it

--- TGD/protected/migrations/m150312_082029_create_interest_categories_cache_and_functions.php
+++ TGD/protected/migrations/m150312_082029_create_interest_categories_cache_and_functions.php
@@ -93,7 +93,61 @@ class m150312_082029_create_interest_categories_cache_and_functions extends CDbM
 
         $this->execute("ALTER FUNCTION _getcategoriesrating(integer, integer, date, date) OWNER TO tgd;");
 
+        
+        $this->execute('
+            CREATE OR REPLACE FUNCTION _getsubcategories(IN id_p integer)
+              RETURNS TABLE(id integer) AS
+            $BODY$
 
+            SELECT id FROM tbl_interest_categories WHERE id = id_p
+            UNION
+                SELECT c1.id
+                FROM tbl_interest_categories c
+                JOIN tbl_interest_categories c1
+                    ON c1.parent_id = c.id
+                WHERE c.id = id_p
+            UNION
+                SELECT c2.id
+                FROM tbl_interest_categories c
+                JOIN tbl_interest_categories c1
+                    ON c1.parent_id = c.id
+                JOIN tbl_interest_categories c2
+                    ON c2.parent_id = c1.id
+                WHERE c.id = id_p
+            UNION
+                SELECT c3.id
+                FROM tbl_interest_categories c
+                JOIN tbl_interest_categories c1
+                    ON c1.parent_id = c.id
+                JOIN tbl_interest_categories c2
+                    ON c2.parent_id = c1.id
+                JOIN tbl_interest_categories c3
+                    ON c3.parent_id = c2.id
+                WHERE c.id = id_p
+            UNION
+                SELECT c4.id
+                FROM tbl_interest_categories c
+                JOIN tbl_interest_categories c1
+                    ON c1.parent_id = c.id
+                JOIN tbl_interest_categories c2
+                    ON c2.parent_id = c1.id
+                JOIN tbl_interest_categories c3
+                    ON c3.parent_id = c2.id
+                JOIN tbl_interest_categories c4
+                    ON c4.parent_id = c3.id
+                WHERE c.id = id_p
+
+            ;
+
+            $BODY$
+              LANGUAGE sql VOLATILE
+              COST 100
+              ROWS 1000;
+        ');
+
+        $this->execute("ALTER FUNCTION _getsubcategories(integer) OWNER TO tgd;");
+
+        
         $this->execute('
             CREATE OR REPLACE FUNCTION _getcategoriesvisitcounter(
                 IN id_p integer,
@@ -155,58 +209,7 @@ class m150312_082029_create_interest_categories_cache_and_functions extends CDbM
 
 
 
-        $this->execute('
-            CREATE OR REPLACE FUNCTION _getsubcategories(IN id_p integer)
-              RETURNS TABLE(id integer) AS
-            $BODY$
-
-            SELECT id FROM tbl_interest_categories WHERE id = id_p
-            UNION
-                SELECT c1.id
-                FROM tbl_interest_categories c
-                JOIN tbl_interest_categories c1
-                    ON c1.parent_id = c.id
-                WHERE c.id = id_p
-            UNION
-                SELECT c2.id
-                FROM tbl_interest_categories c
-                JOIN tbl_interest_categories c1
-                    ON c1.parent_id = c.id
-                JOIN tbl_interest_categories c2
-                    ON c2.parent_id = c1.id
-                WHERE c.id = id_p
-            UNION
-                SELECT c3.id
-                FROM tbl_interest_categories c
-                JOIN tbl_interest_categories c1
-                    ON c1.parent_id = c.id
-                JOIN tbl_interest_categories c2
-                    ON c2.parent_id = c1.id
-                JOIN tbl_interest_categories c3
-                    ON c3.parent_id = c2.id
-                WHERE c.id = id_p
-            UNION
-                SELECT c4.id
-                FROM tbl_interest_categories c
-                JOIN tbl_interest_categories c1
-                    ON c1.parent_id = c.id
-                JOIN tbl_interest_categories c2
-                    ON c2.parent_id = c1.id
-                JOIN tbl_interest_categories c3
-                    ON c3.parent_id = c2.id
-                JOIN tbl_interest_categories c4
-                    ON c4.parent_id = c3.id
-                WHERE c.id = id_p
-
-            ;
-
-            $BODY$
-              LANGUAGE sql VOLATILE
-              COST 100
-              ROWS 1000;
-        ');
-
-        $this->execute("ALTER FUNCTION _getsubcategories(integer) OWNER TO tgd;");
+        
 	}
 
 	public function down()
