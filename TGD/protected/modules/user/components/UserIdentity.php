0d7e96fb245516f42146712be9b50da9584bb122
Alexandru Trandafir Catalin <atrandafirc@gmail.com>
2015-04-04 19:39:02 +0200

    move additional login checks to a common function to use the same rules in different parts of the application

--- TGD/protected/modules/user/components/UserIdentity.php
+++ TGD/protected/modules/user/components/UserIdentity.php
@@ -7,61 +7,73 @@
  */
 class UserIdentity extends CUserIdentity
 {
+  
 	private $_id;
+    
+    // error codes
+    
 	const ERROR_EMAIL_INVALID=3;
 	const ERROR_STATUS_NOTACTIV=4;
 	const ERROR_STATUS_BAN=5;
-        const ERROR_STATUS_APPLIED=6;
+    const ERROR_STATUS_APPLIED=6;
+    
 	/**
 	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
-	 * In practical applications, this should be changed to authenticate
-	 * against some persistent user identity storage (e.g. database).
 	 * @return boolean whether authentication succeeds.
 	 */
 	public function authenticate()
 	{
+        // search by email or by username
 		if (strpos($this->username,"@")) {
-			$user=User::model()->notsafe()->find('LOWER(email)= :email', array(':email' => strtolower($this->username)));
-			
+          $user=User::model()->notsafe()->find('LOWER(email)= :email', array(':email' => strtolower($this->username)));
 		} else {
-
-			$user=User::model()->notsafe()->find('LOWER(username)= :username', array(':username' => strtolower($this->username)));
-
+          $user=User::model()->notsafe()->find('LOWER(username)= :username', array(':username' => strtolower($this->username)));
 		}
-
-		if($user===null)
-		{	
-
-			if (strpos($this->username,"@")) {
-				$this->errorCode=self::ERROR_EMAIL_INVALID;
-			} else {
-				$this->errorCode=self::ERROR_USERNAME_INVALID;
-			}
-		}
-		else if(Yii::app()->getModule('user')->encrypting($this->password)!==$user->password)
+        
+		if($user===null) {
+          if (strpos($this->username,"@")) {
+            $this->errorCode=self::ERROR_EMAIL_INVALID;
+          } else {
+            $this->errorCode=self::ERROR_USERNAME_INVALID;
+          }
+		} else if (Yii::app()->getModule('user')->encrypting($this->password)!==$user->password) {
 			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else if($user->status==0&&Yii::app()->getModule('user')->loginNotActiv==false)
-			$this->errorCode=self::ERROR_STATUS_NOTACTIV;
-		else if($user->status==User::STATUS_APPLIED)
-			$this->errorCode=self::ERROR_STATUS_APPLIED;
-		else if(in_array($user->status, array(
-                    User::STATUS_EXPELLED,
-                    User::STATUS_DENIED,
-                    User::STATUS_LEFT,
-                )))
-			$this->errorCode=self::ERROR_STATUS_BAN;
-		else 
-		{
-			$this->_id=$user->id;
-			$this->username=$user->username;
-			$this->errorCode=self::ERROR_NONE;
+        } else {
+          // do other login checks
+          $this->errorCode=self::getCanLoginError($user);
 		}
-		
+        
+        if ((int)$this->errorCode === self::ERROR_NONE) {
+          $this->_id=$user->id;
+          $this->username=$user->username;
+        }
 
 		return !$this->errorCode;
 	}
+    
+    /**
+     * Does additional login check such as status. This function is used here, when logging in 
+     * but also from other places to verify that a already logged in user can still login.
+     * @param User $user
+     * @return integer Error code
+     */
+    static public function getCanLoginError(&$user) {
+      $errorCode=self::ERROR_NONE;
+      if($user->status==0 && Yii::app()->getModule('user')->loginNotActiv==false) {
+        $errorCode=self::ERROR_STATUS_NOTACTIV;
+      } else if ($user->status==User::STATUS_APPLIED) {
+        $errorCode=self::ERROR_STATUS_APPLIED;
+      } else if (in_array($user->status, array(
+                  User::STATUS_EXPELLED,
+                  User::STATUS_DENIED,
+                  User::STATUS_LEFT,
+              ))) {
+        $errorCode=self::ERROR_STATUS_BAN;
+      } else {
+        $errorCode=self::ERROR_NONE;
+      }
+      return $errorCode;
+    }
     
     /**
     * @return integer the ID of the user record
