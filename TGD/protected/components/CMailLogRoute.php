2cc46c7c4afd988d2fbeb7712c1e861eb0634ca6
Alexandru Trandafir Catalin <atrandafirc@gmail.com>
2015-04-05 19:15:39 +0200

    #120 remove wordwrap and add line breaks for proper formatting

--- TGD/protected/components/CMailLogRoute.php
+++ TGD/protected/components/CMailLogRoute.php
@@ -1,11 +1,23 @@
 <?php
 
 class CMailLogRoute extends CEmailLogRoute {
+  
+    protected function processLogs($logs)
+	{
+		$message='';
+		foreach($logs as $log)
+			$message.=$this->formatLogMessage($log[0],$log[1],$log[2],$log[3]);
+		$subject=$this->getSubject();
+		if($subject===null)
+			$subject=Yii::t('yii','Application Log');
+		foreach($this->getEmails() as $email)
+			$this->sendEmail($email,$subject,$message);
+	}
 
     protected function sendEmail($email, $subject, $text) {
         $message = new YiiMailMessage;
         $message->subject = $subject;
-        $message->setBody($text,'text/html');
+        $message->setBody(nl2br($text),'text/html');
         $message->addTo($email);
         $message->setFrom(Yii::app()->params['marcosEmail'], Yii::app()->params['marcosEmailName']);
         return Yii::app()->mail->send($message);
