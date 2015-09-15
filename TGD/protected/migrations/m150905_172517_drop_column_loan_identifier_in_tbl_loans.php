cbdabf5a5a71b189e69cda877e833f52a51f983d
Juan Menendez <juan.menendez.buitrago@gmail.com>
2015-09-15 15:30:01 +0200

    Migration fix for images whose name were formatted without the loan_identifier prefix.

--- TGD/protected/migrations/m150905_172517_drop_column_loan_identifier_in_tbl_loans.php
+++ TGD/protected/migrations/m150905_172517_drop_column_loan_identifier_in_tbl_loans.php
@@ -12,25 +12,39 @@ class m150905_172517_drop_column_loan_identifier_in_tbl_loans extends CDbMigrati
 			$uploads = Yii::app()->basePath."/../uploads/";
 			foreach($loans as $index => $loan){
 
-				$src = sprintf("%s-%s", $uploads.$loan->loan_identifier, $loan->image);
-				$dest = sprintf("%s-%s", $uploads.$loan->id, $loan->image);
+                $src = sprintf("%s-%s", $uploads.$loan->loan_identifier, $loan->image);
+                $existsSrc = file_exists($src)?"EXISTS":"DOESN'T EXIST";
 
-				print("from: $src\n");
-				print("to  : $dest\n");
+                $src2 = $uploads.$loan->image;
+                $existsSrc2 = file_exists($src2)?"EXISTS":"DOESN'T EXIST";
 
-				if(file_exists($src) && !file_exists($dest)){
-					print("copying files...");
-					print(copy($src, $dest)?"done!\n":"fail!\n");
-					print("changing group...");
-					print(chgrp($dest, "www-data")?"done!\n":"fail!\n");
-					print("changing permissions...");
-					print(chmod($dest, 0664)?"done!\n\n":"fail!\n\n");
-				}else if(!file_exists($src)){
-					print("$src does not exist\n\n");
-					$errorFlag = true;
-				}else{
-					print("$dest already copied\n\n");
-				}
+                $dest = sprintf("%s-%s", $uploads.$loan->id, $loan->image);
+                $existsDest = file_exists($dest)?"EXISTS":"DOESN'T EXIST";
+
+                print("from ($existsSrc): $src\n");
+                print("from ($existsSrc2): $src2\n");
+                print("to ($existsDest): $dest\n");
+
+                if(file_exists($src) && !file_exists($dest)){
+                        print("copying files...");
+                        print(copy($src, $dest)?"done!\n":"fail!\n");
+                        print("changing group...");
+                        print(chgrp($dest, "www-data")?"done!\n":"fail!\n");
+                        print("changing permissions...");
+                        print(chmod($dest, 0664)?"done!\n\n":"fail!\n\n");
+                }else if(file_exists($src2) && !file_exists($dest)){
+                        print("copying files...");
+                        print(copy($src2, $dest)?"done!\n":"fail!\n");
+                        print("changing group...");
+                        print(chgrp($dest, "www-data")?"done!\n":"fail!\n");
+                        print("changing permissions...");
+                        print(chmod($dest, 0664)?"done!\n\n":"fail!\n\n");
+                }else if(!file_exists($src) && !file_exists($src2)){
+                        print("$src does not exist\n\n");
+                        $errorFlag = true;
+                }else{
+                        print("$dest already copied\n\n");
+                }
 			}
 		}
 
