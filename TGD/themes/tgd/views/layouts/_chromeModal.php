5f9d1ca5e64d2031b81fe1fa9aaf3d6c7d371160
Alexandru Trandafir Catalin <atrandafirc@gmail.com>
2015-09-24 20:58:53 +0200

    Put chromeModal in a partial view shared between old and new layout.

---
+++ TGD/themes/tgd/views/layouts/_chromeModal.php
@@ -0,0 +1,32 @@
+<!-- modal chrome only install -->
+<div id="chromeModal" tabindex="-1" role="dialog" aria-labelledby="chromeModal" aria-hidden="true" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="myModalLabel">Browser not supported</h4>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="form-group col-sm-16" id="modal-description">
+                        We only support Chrome for the time being. We would appreciate your help to extend our service to other browsers either by collaborating in our 
+                        <a href="https://github.com/thegooddata">open source developments</a> or 
+                        <a href="<?php echo Yii::app()->controller->createAbsoluteUrl('/donate');?>">giving us a donation.</a>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <form class="form-inline" id="phplist-signup" style="display:none;">
+                    <div class="form-group col-sm-10">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="enter your email">
+                    </div>
+                    <button type="submit" class="btn btn-default col-sm-6 tgd-button tgd-send">Send</button>
+                </form>
+                <button type="button" class="btn btn-default col-sm-6 tgd-button tgd-send" data-dismiss="modal" style="display: none;">Close</button>
+                <a type="button" class="btn btn-default tgd-button tgd-send" href="<?php echo Yii::app()->controller->createAbsoluteUrl("/donate");?>">Continue</a>
+            </div>
+        </div>
+    </div>
+
+</div>
--- TGD/themes/tgd/views/layouts/newMain.php
+++ TGD/themes/tgd/views/layouts/newMain.php
@@ -174,38 +174,8 @@
             </div> 
 
         <?php } ?>
-        <!-- modal chrome only install -->
-        <div id="chromeModal" tabindex="-1" role="dialog" aria-labelledby="chromeModal" aria-hidden="true" class="modal fade">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title" id="myModalLabel">Browser not supported</h4>
-                    </div>
-                    <div class="modal-body">
-                        <div class="row">
-                            <div class="form-group col-sm-16" id="modal-description">
-                                We only support Chrome for the time being. We would appreciate your help to extend our service to other browsers either by collaborating in our 
-                                <a href="https://github.com/thegooddata">open source developments</a> or 
-                                <a href="http://www.tgd.local/index-test.php/donate">giving us a donation.</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <form class="form-inline" id="phplist-signup" style="display:none;">
-                            <div class="form-group col-sm-10">
-                                <label for="email">Email</label>
-                                <input type="email" class="form-control" id="email" placeholder="enter your email">
-                            </div>
-                            <button type="submit" class="btn btn-default col-sm-6 tgd-button tgd-send">Send</button>
-                        </form>
-                        <button type="button" class="btn btn-default col-sm-6 tgd-button tgd-send" data-dismiss="modal" style="display: none;">Close</button>
-                        <a type="button" class="btn btn-default tgd-button tgd-send" href="http://www.tgd.local/index-test.php/donate">Continue</a>
-                    </div>
-                </div>
-            </div>
-
-        </div>
+      
+        <?php $this->renderPartial('_chromeModal'); ?>
 
         <!-- modal chrome install success -->
         <div id="chromeModalInstallSuccess" tabindex="-1" role="dialog" aria-labelledby="chromeModalInstallSuccess" aria-hidden="true" class="modal fade">
