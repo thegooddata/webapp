5e8aeb27f938e031ae3e106400b3f464b2cd1745
Alexandru Trandafir Catalin <atrandafirc@gmail.com>
2017-08-16 17:09:28 +0200

    Notification tab page tweaks.

--- TGD/protected/views/notificationTab/index.php
+++ TGD/protected/views/notificationTab/index.php
@@ -1,3 +1,34 @@
+<?php
+
+$url_path = Yii::app()->getRequest()->getHostInfo().Yii::app()->getRequest()->getRequestUri();
+
+if ( $achievements->image )
+{
+  $img_src = Yii::app()->getRequest()->getHostInfo().'/uploads/'.$achievements->id.'-'.$achievements->image;
+}
+else
+{
+  $img_src = Yii::app()->getRequest()->getHostInfo().Yii::app()->theme->baseUrl.'/img/notification-test.jpg';
+}
+
+$cs=Yii::app()->clientScript;
+
+$cs->registerMetaTag('summary', 'twitter:card');
+$cs->registerMetaTag($this->pageDescription, 'twitter:description');
+$cs->registerMetaTag($img_src, 'twitter:image');
+$cs->registerMetaTag('@thegooddata', 'twitter:site');
+$cs->registerMetaTag($this->pageTitle, 'twitter:title');
+$cs->registerMetaTag($url_path, 'twitter:url');
+
+$cs->registerMetaTag($this->pageDescription, 'og:description');
+$cs->registerMetaTag($img_src, 'og:image');
+$cs->registerMetaTag($this->pageTitle, 'og:title');
+$cs->registerMetaTag('website', 'og:type');
+$cs->registerMetaTag($url_path, 'og:url');
+$cs->registerMetaTag('thegooddata', 'og:site_name');
+
+?>
+
 <section class="ads-leaderboard-container">
   <div class="ads-leaderboard"></div>
 </section>
@@ -23,23 +54,12 @@
     </header>
     <div class="content-container">
       <div class="content-left-column">
-        <?php
-        if ( $achievements->image )
-        {
-          $img_src = '/uploads/'.$achievements->id.'-'.$achievements->image;
-        }
-        else
-        {
-          $img_src = Yii::app()->theme->baseUrl.'/img/notification-test.jpg';
-        }
-        ?>
-        <img src="<?php echo $img_src; ?>"/>
+        <img src="<?php echo $img_src; ?>" />
         <div class="share-container">
           <p>SHARE THIS</p>
           <ul class="social">
-            <?php $url_path = Yii::app()->getRequest()->getHostInfo().Yii::app()->getRequest()->getRequestUri(); ?>
             <li>
-              <a id="facebook" href="http://www.facebook.com/sharer.php?s=100&amp;p[url]=<?php echo $url_path; ?>&amp;p[images][0]=<?php echo Yii::app()->getRequest()->getHostInfo().$img_src ?>&amp;p[title]=TheGoodData | Enjoy your data<?php echo $this->pageTitle;?>&amp;p[summary]=<?php echo $this->pageDescription; ?>" target="_blank"></a>
+              <a id="facebook" href="http://www.facebook.com/sharer.php?s=100&amp;p[url]=<?php echo $url_path; ?>" target="_blank"></a>
             </li>
             <li>
               <a id="twitter" href="https://twitter.com/home?status=<?php echo $url_path; ?>%20TheGoodData%20%7C%20Enjoy%20your%20data" target="_blank"></a>
@@ -49,14 +69,13 @@
             </li>
           </ul>
         </div>
-        
       </div>
       <div class="content-right-column">
         <p>
           <?php echo $achievements->text_en; ?>
           <?php if ( $achievements->link_en ) : ?>
-          <br><br>
-          <a href="<?php echo $achievements->link_en ?>" target="_blank">More information</a>
+            <br><br>
+            <a href="<?php echo $achievements->link_en ?>" target="_blank">More information</a>
           <?php endif; ?> 
         </p>
       </div> 
@@ -75,7 +94,6 @@
           <img class="inline-middle shield" src="<?php echo Yii::app()->theme->baseUrl; ?>/img/ico-box.svg" />
           <span class="shield-text">VALUE</span>
         </div>
-       
         <p class="inline-middle"><span class="shield-number"><?php echo $total_money_earned; ?></span>value collected so far</p>
       </div>
     </div>
@@ -91,12 +109,10 @@
         </a>      -->
         <?php 
         $image = CHtml::image( Yii::app()->theme->baseUrl. '/img/close-yellow-button.svg', 'close', array('class' => 'inline-middle'));
-	echo  CHtml::link($image,'#',array('submit'=>array('notificationtab/deactivateNotification'),'confirm' => 'Are you sure you ?'));       
+        echo  CHtml::link($image,'#',array('submit'=>array('notificationtab/deactivateNotification'),'confirm' => 'Are you sure you ?'));       
         ?>
-
         <p class="inline-middle">Don't show again this notification</p>
       </div>
-      
     </footer>
     
   </article>
