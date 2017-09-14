26ad2123e10c65d7bb46c5e7823d5c7c57af2ed0
Alexandru Trandafir Catalin <atrandafirc@gmail.com>
2017-09-14 15:38:15 +0200

    Adding adsense script into head.

---
+++ TGD/themes/tgd/views/layouts/_adsense_head.php
@@ -0,0 +1,7 @@
+<script async src="//"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-4626593887138860",
+    enable_page_level_ads: true
+  });
+</script>
